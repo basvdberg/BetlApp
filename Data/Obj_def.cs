1cef543d36bf9fc589e98dcbb1c394c1b29e6954
Bas van den Berg <b.vdberg@c2h.nl>
2021-03-04 11:13:27 +0100

    Add project files.

---
+++ Data/Obj_def.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BetlApp.Data
+{
+    [Table("Obj_def",Schema ="dbo")]
+    public class Obj_def
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BetlApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BetlApp.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Obj_def> obj_defs { get; set; }
+    }
+}
